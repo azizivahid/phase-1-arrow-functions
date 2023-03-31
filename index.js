5ce7de3f123a907bc2d695ea8bfa6ecc32787c0d
azizivahid <azizi.vahid@yahoo.com>
2023-03-31 18:40:05 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,7 @@
+function divide (num1 = 2000, num2 = 100){
+    return num1/num2
+}
+
+const square = (parameter1) => parameter1*parameter1
+
+const add = (parameter01,parameter02)=> parameter01+parameter02
